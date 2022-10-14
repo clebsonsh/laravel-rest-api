0c51a3028c6653da83df8bb142b325dce85cf987
Clebson Moura <clebson.sh@gmail.com>
2022-10-14 02:13:32 +0000

    migrations, factories and seeders for Customer and Invoice

--- database/factories/InvoiceFactory.php
+++ database/factories/InvoiceFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -9,15 +10,17 @@ use Illuminate\Database\Eloquent\Factories\Factory;
  */
 class InvoiceFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition()
     {
+        $status = collect(['B', 'P', 'V'])->random();
+
+
         return [
-            //
+            'customer_id' => Customer::factory(),
+            'amount' => $this->faker->numberBetween(100, 20000),
+            'status' => $status,
+            'billed_date' => $this->faker->dateTimeThisDecade(),
+            'paid_date' => $status == 'P' ? $this->faker->dateTimeThisDecade() : NULL,
         ];
     }
 }
