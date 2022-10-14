ce7a31dfa11792385b7c4a21509694a13ed2fbae
Clebson Moura <clebson.sh@gmail.com>
2022-10-14 03:27:34 +0000

    Invoice api routes

--- routes/api.php
+++ routes/api.php
@@ -20,5 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // api/v1
 Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
-    Route::apiResource('customers', CustomerController::class);
+    Route::apiResources([
+        'customers' => CustomerController::class,
+        'invoices' => InvoiceController::class,
+    ]);
 });
