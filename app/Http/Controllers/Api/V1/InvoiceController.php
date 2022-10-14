64c0c5052c92b3c8d14acc8a9aba853cae87c307
Clebson Moura <clebson.sh@gmail.com>
2022-10-14 04:46:02 +0000

    filters for Customer and Invoice

--- app/Http/Controllers/Api/V1/InvoiceController.php
+++ app/Http/Controllers/Api/V1/InvoiceController.php
@@ -2,18 +2,27 @@
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Filters\V1\InvoiceFilter;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreInvoiceRequest;
 use App\Http\Requests\UpdateInvoiceRequest;
 use App\Http\Resources\V1\InvoiceCollection;
-use App\Http\Resources\V1\invoiceResource;
+use App\Http\Resources\V1\InvoiceResource;
 use App\Models\Invoice;
+use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return new InvoiceCollection(Invoice::paginate());
+        $filter = new InvoiceFilter();
+        $queryItems = $filter->transform($request);
+
+        if (count($queryItems) == 0) {
+            return new InvoiceCollection(Invoice::paginate());
+        }
+
+        return new InvoiceCollection(Invoice::where($queryItems)->paginate()->withQueryString());
     }
 
     public function store(StoreInvoiceRequest $request)
@@ -23,7 +32,7 @@ class InvoiceController extends Controller
 
     public function show(Invoice $invoice)
     {
-        return new invoiceResource($invoice);
+        return new InvoiceResource($invoice);
     }
 
     public function update(UpdateInvoiceRequest $request, Invoice $invoice)
